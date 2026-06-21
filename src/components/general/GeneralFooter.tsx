57d33db0faa34e228b78e28adc240b387a0130a8
Laurent Koehler <laurent.koehler@gmail.com>
2026-06-21 03:11:03 +0200

    made GeneralFooter, and some CSS

--- src/components/general/GeneralFooter.tsx
+++ src/components/general/GeneralFooter.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
 function GeneralFooter() {
 
     return (
         <footer className="general-footer">
-            <h2>Zizi</h2>
+            <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                <img src={`${import.meta.env.BASE_URL}assets/rock_logo.png`} alt="Rock Library" className='rock-logo' />
+            </Link>
+            <p>&copy; 2026</p>
         </footer>
     )
 }
